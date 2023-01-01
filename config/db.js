9490785a6b0aa86ecdfd6315d87acf194bf6d436
Mohamed Fawzy <pl.mhmdfawzi@gmail.com>
2023-01-01 10:21:11 +0300

    --

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       // useNewUrlParser: true,
       // useUnifiedTopology: true,
